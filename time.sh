970eeaab3db9f68d53d7ec27d7eb191734edd0f9
NathanCioppa <nathan.cioppa@gmail.com>
2025-06-11 17:14:04 -0400

    refactored time.sh

--- time.sh
+++ time.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+tmpTime="/tmp/statusBarTime"
+[[ -p "$tmpTime" ]] || mkfifo "$tmpTime"
+
 get_time() {
-	printf "$(date '+%-m/%-d %-l:%M%#p')"
+	printf "$(date '+%-m/%-d %-l:%M%#p')" > "$tmpTime"
 }
