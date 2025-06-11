88f6c3d9765dc645c320ddbe9cad3d1e9f9c267e
NathanCioppa <nathan.cioppa@gmail.com>
2025-06-11 17:12:21 -0400

    refactored temperature.sh

--- temperature.sh
+++ temperature.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-cpuTemp=$(($(cat /sys/class/thermal/thermal_zone1/temp) / 1000))
+tmpTemperature="/tmp/temperature"
+[[ -p "$tmpTemperature" ]] || mkfifo "$tmpTemperature"
 
 get_temperature() {
+	cpuTemp=$(($(cat /sys/class/thermal/thermal_zone1/temp) / 1000))
+	
 	if [[ $cpuTemp -ge 90 ]]; then
 		warning=" **"
 	elif [[ $cpuTemp -ge 70 ]]; then
 		warning=" *"
 	fi
 	
-	echo -e "$cpuTemp°C$warning"
+	echo -e "$cpuTemp°C$warning" > "$tmpTemperature"
 }
